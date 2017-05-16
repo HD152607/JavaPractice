04d588995dec316641b779d7e56569c933bd4024
saeim <saeim@DESKTOP-H7HO9E5>
2017-05-16 14:39:04 +0900

    ..

---
+++ JavaPractice/src/org/dimigo/abstractclass/SmartPhoneTest.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package org.dimigo.abstractclass;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *		|_SmartPhoneTest
+ *
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 5. 16.
+ * </pre>
+ *
+ * @author		: saeim
+ * @version		: 1.0
+ *
+ */
+public class SmartPhoneTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SmartPhone[] phone= {
+				new IPhone("iPhone7", "애플", 900000),
+				new Galaxy("갤럭시S8", "삼성", 800000),				
+		};
+		
+		for(SmartPhone p:phone) {
+			System.out.println(p.toString());
+			p.turnOn();
+			p.pay();
+			p.useSpecialFunction();
+			p.turnOff();
+			System.out.println();
+		}
+
+	}
+
+}
